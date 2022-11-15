45db938328a80973461305939cc966a4a0157084
Parth Jindal <pmjindal@gmail.com>
2022-11-15 17:31:14 +0530

    add consent form code

--- app.py
+++ app.py
@@ -4,20 +4,15 @@ from flask_bootstrap import Bootstrap
 from consent import ConsentForm
 from flask import redirect,url_for
 
+from flask import request
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'   
 Bootstrap(app)
 
-
-
-# @app.route("/")
-# def home():
-#     return "Kill me now :("
-
-@app.route("/home")
-def home2():
+@app.route("/home", methods=['GET', 'POST'])
+def home():
     return render_template("home.html", name = "Parth",date="12/12/2019")
-
 
 @app.route("/hello")
 def hello_world():
@@ -25,23 +20,18 @@ def hello_world():
 
 @app.route("/hello/<name>")
 def varfunc(name):
-    return 'Hello %s' % name
-
+    return 'Hello %s, Adios !' % name 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = ConsentForm()
-    message = ""
-    if form.validate_on_submit():
-        accept = form.accept.data
-        name = form.name.data
-        print(name)
-        if accept:
-            return redirect(url_for('home2'))
-        else:
-            message = "That actor is not in our database."
-    return render_template('./index.html', names=form.name.data, form=form, message=message)
-
+    if request.method == 'POST':
+        if form.is_submitted():
+            if form.accept.data == "Yes":
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('varfunc', name = "Parth"))
+    return render_template('consent_form.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug = True) ; 
--- consent.py
+++ consent.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField, BooleanField, RadioField
 from wtforms.validators import DataRequired
 
 class ConsentForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    accept = BooleanField('I accept the terms and conditions', validators=[DataRequired()])
+    accept = RadioField("Please indicate consent before proceeding", choices=[('Yes', 'I consent'), ('No', 'I do not consent')], validators=[DataRequired()])
     submit = SubmitField('Submit')
